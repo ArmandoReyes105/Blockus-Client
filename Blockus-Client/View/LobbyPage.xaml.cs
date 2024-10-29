ebed7686a3d41f6a2c1c4ad2443665b21601251a
x3776x <abrahamvvquinto@gmail.com>
2024-10-28 23:16:30 -0600

    ventana Loobby sin funciones

---
+++ Blockus-Client/View/LobbyPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Blockus_Client.View
+{
+    /// <summary>
+    /// Interaction logic for LobbyPage.xaml
+    /// </summary>
+    public partial class LobbyPage : Page
+    {
+        public LobbyPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Friends_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AccountConfig(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LogOut(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateMatch(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void JoinMatch(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
